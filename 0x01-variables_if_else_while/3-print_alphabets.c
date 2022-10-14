0dd35a3411954e68c617c9f2562657f35d87cec2
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-14 08:23:12 -0700

    alfa

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,10 +12,14 @@ int main(void)
 	char t = '\n';
 	char ph = 'A';
 	
-	for (ch = 'a'; ch  <= 'z'; ch++)                  {                                                         putchar(ch);
+	for (ch = 'a'; ch  <= 'z'; ch++)
+	{
+		putchar(ch);
 	}
 	for (ph = 'A'; ph <= 'Z'; ph++)
 	{
 		putchar(ph);
 	}
-        putchar(t);                                       return (0);                               }
+	putchar(t);
+	return (0);
+}
