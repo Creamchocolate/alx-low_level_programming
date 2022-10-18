f91c25a9305d4d84699c987f62b987539d01b61a
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-18 13:27:17 -0700

    task 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "stdio"
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	long int a, b, c, ch;
+
+	b = 1;
+	c = 2;
+
+	for (a = 1; a <= 50; ++a)
+	{
+		if (b != 20365011074)
+		{
+			printf("%ld, ", b);
+		}
+		else
+		{
+			printf("%ld\n", b);
+		}
+		ch = b + c;
+		b = c;
+		c = ch;
+	}
+	return (0);
+}
