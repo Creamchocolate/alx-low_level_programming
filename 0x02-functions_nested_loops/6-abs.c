a267cb94b778e970dd3990bec2f698723cecebe5
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-18 07:41:39 -0700

    task 06

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int _abs(int)
+{
+	int r;
+	
+	if (r == -1 || r == 1)
+	{
+		n = (r * r);
+	}
+	else
+	{
+		n = ((r * r) / 2);
+	}
+}
