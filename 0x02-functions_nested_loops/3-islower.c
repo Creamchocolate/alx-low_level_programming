478d4da351ac3dd8c8b961d8bef9c4627aecbcd1
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-18 06:41:32 -0700

    task 03

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _islower - Entry point
- *
+ * c - is any character
  * Return: Always 0 (sucecess)
  */
 
 int _islower(int c)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (c == ch)
