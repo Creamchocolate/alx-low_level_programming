bee54d79abf5f3cde8a789efe7478d905e794f00
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-18 05:03:54 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	_putchar('m');
+	_putchar('a');
+	_putchar('i');
+	_putchar('n');
+	_purchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,2 @@
+void _putchar(void);
+void print_alphabet(void);
