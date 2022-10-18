976d2a8655023714b47c05717191bbe64402f3a9
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-18 10:10:20 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - asds two argument
+ * @a: first argument
+ * @b: second argument
+ * Return: Always 0 (success)
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
