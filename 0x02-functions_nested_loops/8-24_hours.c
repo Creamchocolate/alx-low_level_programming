7698bb2da6e94eaf78ee0a65a20f39400e143db8
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-18 09:30:14 -0700

    task 08

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+void jack_bauer(void)
+{
+	int m, n;
+	m = 0;
+
+	while (m < 24)
+	{
+		n = 0;
+
+		while (n < 60)
+		{
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar(':');
+			_putchar((n / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			n++;
+		}
+		m++;
+	}
+}
