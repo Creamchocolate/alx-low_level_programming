f9cab09a354f65262becb5109723d964f3a5e3c6
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-20 03:57:37 -0700

    task 02

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplt two integers
+ * @a: integer one
+ * @b: integer two
+ * Return: Always 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	int Z;
+
+	Z = a * b;
+	_putchar(Z);
+}
