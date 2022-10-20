96729f40361200ad108ba77a59609cdf243d8bec
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-20 03:53:00 -0700

    task 01

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digit
+ * @c: is an integer
+ * Return: 1 for integer, 0 else
+ */
+
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (c == i)
+		{
+			Return 1;
+		}
+		else
+		{
+			Return 0;
+		}
+	}
+}
