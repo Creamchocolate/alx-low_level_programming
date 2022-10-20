5db7a2858e880454a75883120fe6acb6e976177d
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-20 04:03:02 -0700

    task 03

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - numbers between 0-9
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	Return 0;
+}
