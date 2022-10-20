704a9c77dee4c6176604b781ad4702f023be7c73
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-20 03:33:40 -0700

    task 00

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isupper - checks for upper case letters
+ * @c: characters
+ * Return: 1 for uppercase, 0 else
+ */
+
+int _isupper(int c)
+{
+	char i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (c == i)
+		{
+			Return 1;
+		}
+		else
+		{
+			Return 0;
+		}
+	}
+}
