6614df39728bce3cda6a3d7b4731cbe26ed79d2a
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-20 06:17:13 -0700

    task 05

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,16 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	i = 0;
-
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
+			if (j >= 10)
+				_putchar('1');
+			_putchar((j % 10) + '0');
 		}
-		i++;
+		_putchar('\n');
 	}
-	_putchar('\n');
-	Return 0;
 }
