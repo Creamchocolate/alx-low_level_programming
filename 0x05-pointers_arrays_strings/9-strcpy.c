2180e5e7368d5efd9be61b48696b7e285b89f027
Ediongseyene Isaac <isaacediong@gmail.com>
2022-10-24 04:39:35 -0700

    task 09

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_strcpy - copies string from one variable to another
+ * @dest: pointer one
+ * @src: pointer two
+ * Return: Always 0 (success)
+ */
+
+char *_strcpy(char *dest, char *src);
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
+	return (dest);
+}
